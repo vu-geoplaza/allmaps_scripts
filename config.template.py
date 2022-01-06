07d205f5748e8e3795b848f6a66e8ff986602825
peer35 <p.j.m.vos@vu.nl>
2022-01-06 11:32:16 +0100

    cleanup

--- config.template.py
+++ config.template.py
@@ -3,3 +3,5 @@ OMO_URL='https://api.oldmapsonline.org/1.0/'
 
 CDM_BASEURL = 'https://server21033.contentdm.oclc.org'  # CONTENTdm backend URL
 CDM_APIURL = CDM_BASEURL + '/dmwebservices/index.php'
+CDM_REF_BASE = 'https://vu.contentdm.oclc.org/digital/collection/krt/id/'
+CDM_IIIF_BASE = 'https://vu.contentdm.oclc.org/digital/iiif/krt/'
--- export_cdm_list.py
+++ export_cdm_list.py
@@ -1,17 +1,8 @@
 import CdmApi
 import requests_cache, json
+from config import CDM_REF_BASE, CDM_IIIF_BASE
 
 requests_cache.install_cache('requests_cache')
-# 3557: landgoedkaarten
-# 6743, 2131: tmk
-# Globes: 3748, 3769, 3727, 3832, 3866, 3811, 3853, 3790,3618,3794
-IGNORE_LIST = [3748, 3769, 3727, 3832, 3866, 3811, 3853, 3790,
-               3618, 3794]  # compound or single records that should be skipped
-IGNORE_PAGE_TITLE_LIST = ['[indexkaart]']
-NO_DEEPZOOM = [7244]
-REF_BASE = 'https://vu.contentdm.oclc.org/digital/collection/krt/id/'
-IIIF_BASE = 'https://vu.contentdm.oclc.org/digital/iiif/krt/'
-#DZ_BASE = 'http://imagebase.ubvu.vu.nl/cdm/deepzoom/collection/krt/id/'
 
 
 def sanitize(val):
@@ -43,13 +34,12 @@ def convert(metadata, pagemetadata):
     row['id'] = ubvuid
     dmrecord = pagemetadata['dmrecord'] if pagemetadata else metadata['dmrecord']
     row['dmrecord'] = dmrecord
-    row['link'] = '%s%s' % (REF_BASE, dmrecord)
-    row['iiif'] = '%s%s' % (IIIF_BASE, dmrecord)
+    row['link'] = '%s%s' % (CDM_REF_BASE, dmrecord)
+    row['iiif'] = '%s%s' % (CDM_IIIF_BASE, dmrecord)
     #row['title'] = '%s, uit: %s' % (pagemetadata['title'], metadata['title']) if pagemetadata else metadata['title']
 
-    if (pagemetadata and pagemetadata['title'].lower() in IGNORE_PAGE_TITLE_LIST) or ubvuid == '':
+    if ubvuid == '':
         row = False
-
     return row
 
 
@@ -62,39 +52,38 @@ data=[]
 # loop through ptrs and get metadata
 for ptr in ptrs:
     print(ptr)
-    if ptr not in IGNORE_LIST:
-        metadata = CdmApi.getMetadata(nick, ptr)
-        if 'code' not in metadata:  # some broken items?
-            if CdmApi.isCpd(nick, ptr):
-                cpd = CdmApi.getCpdPages(nick, ptr)
-                n = 0
-                if cpd['type'] != 'Monograph':
-                    for page in cpd['page']:
+    metadata = CdmApi.getMetadata(nick, ptr)
+    if 'code' not in metadata:  # some broken items?
+        if CdmApi.isCpd(nick, ptr):
+            cpd = CdmApi.getCpdPages(nick, ptr)
+            n = 0
+            if cpd['type'] != 'Monograph':
+                for page in cpd['page']:
+                    pageptr = page['pageptr']
+                    page_metadata = CdmApi.getMetadata(nick, pageptr)
+                    row = convert(metadata, page_metadata)
+                    if row:
+                        data.append(row)
+            else:
+                for node in cpd['node']['node']:  # specific case of tmk, could be deeper
+                    if type(node['page']) is dict:  # what a shitty data structure
+                        page = node['page']
                         pageptr = page['pageptr']
                         page_metadata = CdmApi.getMetadata(nick, pageptr)
-                        row = convert(metadata, page_metadata)
-                        if row:
-                            data.append(row)
-                else:
-                    for node in cpd['node']['node']:  # specific case of tmk, could be deeper
-                        if type(node['page']) is dict:  # what a shitty data structure
-                            page = node['page']
+                    row = convert(metadata, page_metadata)
+                    if row:
+                        data.append(row)
+                    else:
+                        for page in node['page']:
                             pageptr = page['pageptr']
                             page_metadata = CdmApi.getMetadata(nick, pageptr)
-                        row = convert(metadata, page_metadata)
-                        if row:
-                            data.append(row)
-                        else:
-                            for page in node['page']:
-                                pageptr = page['pageptr']
-                                page_metadata = CdmApi.getMetadata(nick, pageptr)
-                                row = convert(metadata, page_metadata)
-                                if row:
-                                    data.append(row)
-            else:
-                row = convert(metadata, False)
-                if row:
-                    data.append(row)
+                            row = convert(metadata, page_metadata)
+                            if row:
+                                data.append(row)
+        else:
+            row = convert(metadata, False)
+            if row:
+                data.append(row)
 
 
 with open('cdm_export.json', 'w') as f:
