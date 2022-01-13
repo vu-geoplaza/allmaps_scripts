c846fbcc6e7191f9fe5fa196a276d64faffbc821
peer35 <peter@pvsge076.labs.vu.nl>
2022-01-13 12:02:27 +0100

    scheduled update

---
+++ update_allmaps_annotations.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd ~/allmaps_scripts
+git pull
+source ~/geo_venv/bin/activate
+#python export_omo.py
+rm -f annotations_unreviewed/*
+python create_annotations.py
+git add .
+git commit -m 'scheduled update' .
+git push
